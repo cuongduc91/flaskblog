295cbe34b60da2147d1757c5a97fa8cca99c997d
Cuong Bui Duc <buiducc@Fida.local>
2020-04-30 11:55:47 +0200

    User Profile pictures and update form

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,10 +1,11 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import BooleanField, StringField, PasswordField, SubmitField
 from wtforms.fields.html5 import EmailField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 import email_validator
 from flaskblog.models import User
-
+from flask_login import current_user
 class RegistrationForm(FlaskForm):
     username=StringField('Username',
     validators=[DataRequired(),Length(min=2,max=20)])
@@ -31,3 +32,24 @@ class LoginForm(FlaskForm):
     password=PasswordField('Password',validators=[DataRequired()])
     remember=BooleanField('Remember me')
     submit = SubmitField('Login')
+
+class UpdateAccountForm(FlaskForm):
+    username=StringField('Username',
+    validators=[DataRequired(),Length(min=2,max=20)])
+    email = EmailField('Email',
+                        validators=[DataRequired(), Email()])
+    picture = FileField('Update profile picture',validators=[FileAllowed(['jpg','png'])])
+    submit = SubmitField('Update')
+
+    #Validation of this form
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('Username already exists')
+    #Validation of this form
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('Email already exists')
